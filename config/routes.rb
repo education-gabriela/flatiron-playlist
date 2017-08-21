caec2123d1025a21611e0d91228b2fe853cbb420
Brigit Rossbach <brigitrossbach@gmail.com>
2017-08-21 15:59:15 -0400

    added playlist and user routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@ Rails.application.routes.draw do
   resources :artists, except: [:destroy]
   resources :genres, except: [:destroy]
   resources :songs, except: [:destroy]
+  resources :users, except: [:destroy, :index]
+  resources :playlists
   post '/login', to: 'sessions#create'
   delete '/sessions/destroy', to: 'sessions#destroy'
   root 'welcome#index'
