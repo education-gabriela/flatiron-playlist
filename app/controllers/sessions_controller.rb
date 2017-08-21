c96d72e002f161f7cc46a604f479d53d2fbef3b2
gabidavila <gabidavila@gmail.com>
2017-08-21 17:01:25 -0400

    applied css to forms

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,22 +1,22 @@
 class SessionsController<ApplicationController
 
-def new
-end
-
-def create
-  @user = User.find_by(email: params[:user][:email])
-  if @user && @user.authenticate(params[:user][:password])
-    session[:user_id] = @user.id
-    redirect_to root_path
-  else
-    render :'users/new'
+  def new
   end
-end
 
-def destroy
-  session.clear
-  redirect_to root_path
-end
+  def create
+    @user = User.find_by(email: params[:user][:email])
+    if @user && @user.authenticate(params[:user][:password])
+      session[:user_id] = @user.id
+      redirect_to root_path
+    else
+      render :'users/new'
+    end
+  end
+
+  def destroy
+    session.clear
+    redirect_to root_path
+  end
 
 
 end
