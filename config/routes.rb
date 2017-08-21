b1bd15feeefa9263f287d2df872f4bacc0186ab8
gabidavila <gabidavila@gmail.com>
2017-08-21 15:51:06 -0400

    remove destroy out of routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
-  resources :artists
-  resources :genres
-  resources :songs
+  resources :artists, except: [:destroy]
+  resources :genres, except: [:destroy]
+  resources :songs, except: [:destroy]
   post '/login', to: 'sessions#create'
   delete '/sessions/destroy', to: 'sessions#destroy'
   root 'welcome#index'
