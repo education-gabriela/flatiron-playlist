23c6eb00d322f8621e661b385b5d0c2816852466
Brigit Rossbach <brigitrossbach@gmail.com>
2017-08-25 14:06:28 -0400

    home page addition of image

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -2,10 +2,12 @@ class Comment < ApplicationRecord
   belongs_to :commentable, :polymorphic => true
   has_many :comments, :as => :commentable
   belongs_to :user
+  include PublicActivity::Model
+  tracked only: [:new, :create], owner: :user
 
   def playlist
     return @playlist if defined?(@playlist)
     @playlist = commentable.is_a?(Playlist) ? commentable : commentable.playlist
   end
-  
+
 end
